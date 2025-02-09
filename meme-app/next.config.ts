78e6ee85c164816570d120bb82e46efb2de848fe
nkien0204 <nkien0204@gmail.com>
2025-02-09 15:42:25 +0700

    build UI

--- meme-app/next.config.ts
+++ meme-app/next.config.ts
@@ -3,12 +3,16 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    domains: ['plus.unsplash.com', 'upload.wikimedia.org'],
+    domains: [
+      "plus.unsplash.com",
+      "upload.wikimedia.org",
+      "cdn-icons-png.flaticon.com",
+    ],
   },
-  webpack: config => {
-    config.externals.push('pino-pretty', 'lokijs', 'encoding')
-    return config
-  }
+  webpack: (config) => {
+    config.externals.push("pino-pretty", "lokijs", "encoding");
+    return config;
+  },
 };
 
 export default nextConfig;
